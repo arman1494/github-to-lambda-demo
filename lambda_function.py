4937f69dc1ce16909db3933de725f512c759d868
Arman Mansouri <info.zka.co@gmail.com>
2023-08-21 12:50:32 +0300

    Test the Lambda

--- lambda_function.py
+++ lambda_function.py
@@ -9,3 +9,6 @@ def lambda_handler(event, context):
         context (_type_): _description_
     """
     data = {'col1':[1,2], 'col2':[3,4]}
+    df = pd.DataFrame(data=data)
+    print(df)
+    print('Done!')
