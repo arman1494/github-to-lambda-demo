78394afa8d239eccabd6e9528ab41d5b4e172c49
Arman Mansouri <info.zka.co@gmail.com>
2023-08-21 11:47:23 +0300

    Added demo

---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+
+def lambda_handler(event, context):
+    """_summary_
+
+    Args:
+        event (_type_): _description_
+        context (_type_): _description_
+    """
+    data = {'col1':[1,2], 'col2':[3,4]}
